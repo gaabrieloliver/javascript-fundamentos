ecd4cc015045fc9c1071a60d6cb47f810aea013d
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-09-01 23:38:38 -0300

    :tada: variaveis

---
+++ javascript/variaveis/script.js
@@ -0,0 +1,23 @@
+// Declarar uma variável com o seu nome
+var nome = 'Gabriel';
+
+// Declarar uma variável com a sua diade
+var cidade = 'João Pessoa';
+
+// Declarar uma variável com a sua comida 
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'Comida Chinesa';
+
+// Declarar 5 variáveis diferentes sem valores
+var idade, 
+    musica, 
+    hobby, 
+    job; 
+
+
+
+
+
