3d49e58a8c4ac6803dc0c0822387d1deabd01ea6
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-09-13 12:14:08 -0300

    tipos de dados

---
+++ javascript/tipos-de-dados/script.js
@@ -0,0 +1,20 @@
+
+// Declare uma variável contendo uma String
+var nome = 'Gabriel';
+
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2024'
+
+// Declare uma variável com sua idade
+var idade = 28;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Oliveira';
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+var itsTime = "It's' time";
+ 
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
