763fef813a8f4756dc0abae90f88c37928696504
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-09-18 10:23:15 -0300

    Update script.js

--- javascript/boolean-e-condicionais/script.js
+++ javascript/boolean-e-condicionais/script.js
@@ -1,5 +1,13 @@
-var possuiGraduacao = true;
+if((5 - 5) && (5 + 5)) {
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
 
-if(true) {
-    console.log('É verdadeiro');
-} 
+var condicional = (5 - 10) && (5 + 5);
+
+if(condicional) {
+    console.log('Verdadeiro', condicional);
+} else {
+    console.log('Falso');
+}
